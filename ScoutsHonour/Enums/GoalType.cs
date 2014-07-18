fdab04cc790e4af57648edefb86d5b7f4310230d
Luke Ballantine <lukesinbox@gmail.com>
2014-07-18 22:41:08 +1200

    Update Goal to handle new badge structure

---
+++ ScoutsHonour/Enums/GoalType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoutsHonour.Enums
+{
+    public enum GoalType
+    {
+        CubCornerstone = 1,
+        CubPersonal = 2
+    }
+}
--- ScoutsHonour/Models/Goal.cs
+++ ScoutsHonour/Models/Goal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScoutsHonour.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,9 +12,15 @@ namespace ScoutsHonour.Models
         public int Level { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public GoalType GoalType { get; set; }
         public int? ChildRequirementCount { get; set; }
+        public int? Level1ChildRequirementCount { get; set; }
+        public int? Level2ChildRequirementCount { get; set; }
+        public int? Level3ChildRequirementCount { get; set; }
+        public int? GoalLevel { get; set; }
         public virtual ICollection<Goal> Goals { get; set; }
         public virtual ICollection<Member> Members { get; set; }
         public virtual ICollection<Event> Events { get; set; }
+        public virtual Organisation Organisation { get; set; }
     }
 }
