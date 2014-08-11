19f31a5bb5f4233f6f3ce8188881ecbcf7a3dc1f
Luke Ballantine <lukesinbox@gmail.com>
2014-08-12 08:33:18 +1200

    Add demo accounts for cubs NZ

--- ScoutsHonour/App_Data/SqlScripts/AzureSeed.sql
+++ ScoutsHonour/App_Data/SqlScripts/AzureSeed.sql
@@ -1,4 +1,4 @@
-﻿-- Use this in SQL Azure (if Update-Database doesn't work)
+﻿-- This can be run in SQL Azure (only if Update-Database doesn't work)
 
 INSERT INTO [dbo].[AspNetRoles] ([Id],[Name]) VALUES (NEWID(),'Admin')
 INSERT INTO [dbo].[AspNetRoles] ([Id],[Name]) VALUES (NEWID(),'Leader')
@@ -12,6 +12,6 @@ DECLARE @lastId int
 SET @lastId = SCOPE_IDENTITY;
 
 INSERT INTO [dbo].[Groups] ([Title],[GroupCodeParent],[GroupCodeLeader],[Organisation_Id])
-     VALUES ('Glendowie Cubs Tuesday', 'glencubtue', 'glencubtuel', @lastId) 
+     VALUES ('Demo Cubs Tuesday', '111111', '222222', @lastId) 
 INSERT INTO [dbo].[Groups] ([Title],[GroupCodeParent],[GroupCodeLeader],[Organisation_Id])
-     VALUES ('Glendowie Cubs Wednesday', 'glencubwed', 'glencubwedl', @lastId) 
+     VALUES ('Demo Cubs Wednesday', '333333', '444444', @lastId) 
