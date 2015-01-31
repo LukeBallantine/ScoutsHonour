1dd748abade65f0482fb72419256ef3f7faf9ff5
LukeBallantine <lukesinbox@gmail.com>
2015-01-31 15:53:20 +1300

    Add DOB, Age and Icon for Cub Rank

---
+++ ScoutsHonour/Helpers/MemberHelper.cs
@@ -0,0 +1,35 @@
+﻿using ScoutsHonour.Enums;
+using ScoutsHonour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace ScoutsHonour.Helpers
+{
+    public static class MemberHelper
+    {
+        public static string GetRankIcon(string rank)
+        {
+            if (rank == null)
+                return string.Empty;
+
+            if (rank.ToLower() == "sixer") 
+            {
+                return "<i class=\"fa fa-star\" title=\"" + rank + "\"></i>";
+            }
+            else if (rank.ToLower() == "seconder")
+            {
+                return "<i class=\"fa fa-star-half-o\" title=\"" + rank + "\"></i>";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+    }
+}
